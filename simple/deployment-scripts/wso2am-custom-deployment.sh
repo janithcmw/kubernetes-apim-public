2b28e9def24f366832726bf76a6c87d27aa00360
janithcmw <janithcmw@gmail.com>
2023-04-07 18:08:34 +0530

    add custom deployment script.

---
+++ simple/deployment-scripts/wso2am-custom-deployment.sh
@@ -0,0 +1,19 @@
+#create namespace
+kubectl create namespace wso2
+
+# set namespace
+kubectl config set-context $(kubectl config current-context) --namespace=wso2
+
+# create ServiceAccount
+kubectl create -f kubernetes-basics/svcaccount.yaml
+
+# databases
+echo 'deploying databases ...'
+kubectl create -f kubernetes-apim-mysql/wso2apim-mysql-conf.yaml
+kubectl create -f kubernetes-apim-mysql/wso2apim-mysql-service.yaml
+kubectl create -f kubernetes-apim-mysql/wso2apim-mysql-deployment.yaml
+
+# APIM-AllinOne
+kubectl create -f kubernetes-apim/wso2apim-conf.yaml
+kubectl create -f kubernetes-apim/wso2apim-service.yaml
+kubectl create -f kubernetes-apim/wso2apim-deployment.yaml
