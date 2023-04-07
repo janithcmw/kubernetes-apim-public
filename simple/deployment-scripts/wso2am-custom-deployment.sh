d78e4a930f122833737ad32d4213bf85cbc62262
janithcmw <janithcmw@gmail.com>
2023-04-07 20:11:54 +0530

    add ingress annotation for certs.

--- simple/deployment-scripts/wso2am-custom-deployment.sh
+++ simple/deployment-scripts/wso2am-custom-deployment.sh
@@ -18,3 +18,6 @@ kubectl create -f kubernetes-apim/wso2apim-conf.yaml
 kubectl create -f kubernetes-apim/wso2apim-conf-entrypoint.yaml
 kubectl create -f kubernetes-apim/wso2apim-service.yaml
 kubectl create -f kubernetes-apim/wso2apim-deployment.yaml
+
+#Ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
