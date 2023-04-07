7723effa4936af1fff00f83ca3b3fbd9aa1d0cdb
janithcmw <janithcmw@gmail.com>
2023-04-07 20:18:43 +0530

    update ingress configurations.

--- simple/deployment-scripts/wso2am-custom-deployment.sh
+++ simple/deployment-scripts/wso2am-custom-deployment.sh
@@ -17,7 +17,9 @@ kubectl create -f kubernetes-apim-mysql/wso2apim-mysql-deployment.yaml
 kubectl create -f kubernetes-apim/wso2apim-conf.yaml
 kubectl create -f kubernetes-apim/wso2apim-conf-entrypoint.yaml
 kubectl create -f kubernetes-apim/wso2apim-service.yaml
+kubectl create -f kubernetes-apim/wso2apim-gateway-service.yaml
 kubectl create -f kubernetes-apim/wso2apim-deployment.yaml
 
 #Ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
+kubectl create -f kubernetes-apim-ingresses/wso2apim-ingress.yaml
