b17ad520133c8ac3fcc1b5ca6306ad827f93bc5b
janithcmw <janithcmw@gmail.com>
2023-04-12 18:56:29 +0530

    add commands as logs.

--- simple/deployment-scripts/wso2am-custom-deployment.sh
+++ simple/deployment-scripts/wso2am-custom-deployment.sh
@@ -35,3 +35,16 @@ kubectl create -f kubernetes-apim/wso2apim-deployment.yaml
 #Ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
 kubectl create -f kubernetes-apim-ingresses/wso2apim-ingress.yaml
+
+##kubectl commands
+##to exec to wso2apim
+#kubectl exec -it <pod name> bash
+##to exec to netshoot
+#kubectl exec -it <pod name> bash -c netshoot
+##then using the following command the tcp dumps can be captured.
+#tcpdump -i any  -w test.pcap
+##to get the tcp dump using docker cp
+#kubectl cp wso2/wso2am-pattern-1-am-1-deployment-75975469f5-4nnxn:/root/netstat-data/test.pcap /home/janithw/data/test.pcap -c netshoot
+##to view the logs as tail
+#kubectl logs -f <pod name>
+
