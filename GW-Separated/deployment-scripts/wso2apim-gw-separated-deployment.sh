d63cab20c9c02d16366408d3f827d4eb5915f5ab
janithcmw <janithcmw@gmail.com>
2023-10-30 18:12:07 +0530

    fix path of ingress.

--- GW-Separated/deployment-scripts/wso2apim-gw-separated-deployment.sh
+++ GW-Separated/deployment-scripts/wso2apim-gw-separated-deployment.sh
@@ -38,7 +38,7 @@ kubectl create -f kubernetes-apim/GW/wso2apim-gw-separated-gw-deployment.yaml
 
 #Ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
-kubectl create -f kubernetes-apim-ingresses/wso2apim-ingress.yaml
+kubectl create -f kubernetes-ingress/Ingress/wso2apim-gw-separated-ingress.yaml
 
 ##kubectl commands
 ##to exec to wso2apim
